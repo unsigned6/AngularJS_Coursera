db43fb84270ab06c6d6f71661f2e6eb165a4ed46
unsigned6 <yuriivlasiuk@gmail.com>
2016-09-11 00:17:03 +0300

    Solved.

--- module1_solution/scripts/app.js
+++ module1_solution/scripts/app.js
@@ -1,7 +1,31 @@
 (function () {
-  angular.module('LunchCheck', [])
-  .controller('LunchCheckController', LunchCheckController);
+'use strict';
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
 
-  LunchCheckController.$inject = ['$scope'];
-};
-)();
+LunchCheckController.$inject = ['$scope'];
+
+function LunchCheckController($scope) {
+  $scope.numberMenuItems = 0;
+  $scope.checkList = function () {
+    if(typeof($scope.lunchMenuList) == 'undefined' || $scope.lunchMenuList.trim() == "") {
+      $scope.message = "Please enter data first";
+      $scope.msgColor = "red";
+      $scope.fieldColor = "has-error"
+    } else {
+      $scope.msgColor = "green";
+      $scope.fieldColor = "has-success"
+      if (countMenuItems() <= 3) {
+        $scope.message = "Enjoy!";
+      } else {
+        $scope.message = "Too much!";
+      }
+    }
+  };
+
+  function countMenuItems() {
+    return $scope.lunchMenuList.trim().split(",").length;
+  }
+}
+
+})();
